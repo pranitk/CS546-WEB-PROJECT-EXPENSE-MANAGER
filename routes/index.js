da1162aa8f565de8f94973b2357529e37e269bab
Pranit Kulkarni <pranitkulkarni24@gmail.com>
2017-11-14 21:11:08 -0500

    Added Header page interface

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,10 @@ const path = require("path")
 const constructorMethod = (app) => {
 
     app.get("/",(req,res) => {
+        res.sendFile(path.resolve("static/header-material.html"))
+    })
+
+    app.get("/add_expense",(req,res) => {
         res.sendFile(path.resolve("static/add_expense.html"))
     })
 
