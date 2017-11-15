0606d64fe7b186376ed83baa0a92f0e729234f29
sourabh kulkarni <skulkar6@stevens.edu>
2017-11-15 16:09:37 -0500

    login route and mongoose install

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,11 @@ const path = require("path")
 
 const constructorMethod = (app) => {
 
-    app.get("/",(req,res) => {
-        res.sendFile(path.resolve("static/add_expense.html"))
-    })
+    // app.get("/",(req,res) => {
+    //     res.sendFile(path.resolve("static/add_expense.html"))
+    // })
 
-    app.get("/login",(req,res) => {
+    app.get("/",(req,res) => {
         res.sendFile(path.resolve("static/login.html"))
     })
 
---
+++ routes/loginRoute.js
@@ -0,0 +1,41 @@
+var express = require("express");
+var router = express.Router();
+var expressValidator = require("express-validator");
+router.use(expressValidator());
+
+
+//Get Home Page
+router.get("/",(req,res) => {
+    res.render("login");
+});
+
+
+router.post("/login", (req, res) => {
+    var username = req.body.unm;
+    var password = req.body.pswd;
+    req.checkBody("unm","username is required").notEmpty();
+    req.checkBody("pwd","Please enter password").notEmpty();
+
+    var errors = req.validationErrors()
+    if(errors){
+        res.render("register", {
+            errors: error
+        })
+    }else{
+        var newUser = new User({
+            username : username,
+            password : password,
+            
+        })
+
+        User.createUser(newUser,function(err,user){
+            if(err) throw err;
+            console.log(user);
+        })
+
+        req.flash("success_msg","You are registered and can now login");
+        res.redirect("/home");
+    }
+})
+
+module.exports = router;
