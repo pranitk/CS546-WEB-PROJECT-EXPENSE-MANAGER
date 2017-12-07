a4a76fbc3ac325d1a1fc8bf81a7a1c0703948700
Shreyas Sule <suleshreyas7@gmail.com>
2017-12-07 18:03:50 -0500

    user session

--- routes/expenseRoutes.js
+++ routes/expenseRoutes.js
@@ -110,7 +110,7 @@ router.post("/saveNewExpense",async(req,res)=>{
 //Show the add expense page.
 router.get("/addExpense",async(req,res)=>{
     console.log("Add expense get page route called")
-    let bank_accounts = await bankData.getAllAccounts(req.session.user._id)
+    let bank_accounts = await bankData.getAllAccounts(loggedUser);
 
     res.render('transactions/add_expense',{ bank_accounts: bank_accounts })  // handlebar
 })
